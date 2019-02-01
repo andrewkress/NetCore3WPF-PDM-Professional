dbba1e2bc47f4fca0fd5151a4778a99ef66128ac
Andrew Kress <andrew.kress@gmail.com>
2019-02-01 11:31:24 -0600

    Add project files.

---
+++ NetCore3WPF/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using EPDM.Interop.epdm;
+
+namespace NetCore3WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private IEdmVault5 vault;
+        public MainWindow() {
+            InitializeComponent();
+            vault = new EdmVault5();
+        }
+
+        private void ButtonExit_Click(object sender, RoutedEventArgs e) {
+            Application.Current.Shutdown();
+        }
+
+        private void ButtonLogin_Click(object sender, RoutedEventArgs e) {
+            if(!vault.IsLoggedIn) {
+                vault.LoginAuto("training", 0);
+                MessageBox.Show($"Logged in to vault: {vault.Name} with a root of {vault.RootFolderPath}");
+            }
+        }
+    }
+}
